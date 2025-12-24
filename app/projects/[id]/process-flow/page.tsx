1f92b7dc9b32f9dbe6c030997135d5d358461567
stcstorages <stcstorages@gmail.com>
2025-12-24 11:33:27 +0800

    Update page.tsx

--- app/projects/[id]/process-flow/page.tsx
+++ app/projects/[id]/process-flow/page.tsx
@@ -101,7 +101,7 @@ export default async function ProcessFlowPage({
                       <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path></svg>
                     </button>
 
-                    {/* Delete Button (Trash Icon) - Uses formAction to override the main submit */}
+                    {/* Delete Button (Trash Icon) - Uses formAction to override the main submitt */}
                     <button 
                       formAction={deleteProcessStep} 
                       className="p-1 text-red-400 hover:text-red-600 hover:bg-red-50 rounded" 
